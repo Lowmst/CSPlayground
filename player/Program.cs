174bd32c9af5daf23b9aff08db65ad7bc31a77cc
Lowmst <im.liuhongyuan@outlook.com>
2024-11-11 12:35:17 +0800

    naudio wasapi demo

--- player/Program.cs
+++ player/Program.cs
@@ -1,3 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-// Roadmap (NAudio, P/Invoke, FFmpeg) -> CLI -> WinUI3
+﻿using NAudio;
+using NAudio.CoreAudioApi;
+using NAudio.MediaFoundation;
+using NAudio.Wave;
+using System.CommandLine;
+
+namespace player
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                System.Console.WriteLine("Usage: player <filename>");
+                return;
+            }
+
+            var wasapiOut = new WasapiOut();
+            var audioFile = new AudioFileReader(args[0]);
+            wasapiOut.Init(audioFile);
+            wasapiOut.Play();
+
+            while (wasapiOut.PlaybackState == PlaybackState.Playing)
+            {
+                Task.Delay(1);
+            }
+        }
+
+
+    }
+}
